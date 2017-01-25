bc6f3bdc9a6423c807b3cca30cfef134adba0ca4
ajarnchelsea <miller.chelsealauren@gmail.com>
2017-01-25 09:07:33 -0500

    updating png

---
+++ Desktop/desktop/updated portfolio/chelseamiller/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import webapp2
+import jinja2
+import logging
+from webapp2_extras.routes import RedirectRoute
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+	loader = jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        title = "HOME"
+        template_vars={'title':title,'name':'index'}	
+    	template = JINJA_ENVIRONMENT.get_template('base.html')
+        self.response.out.write(template.render(template_vars))
+
+class PhotosHandler(webapp2.RequestHandler):
+    def get(self):
+        template_vars={'title':'PHOTOS','name':'photos'}	
+    	template = JINJA_ENVIRONMENT.get_template('photos.html')
+        self.response.out.write(template.render(template_vars))
+
+class ResumeHandler(webapp2.RequestHandler):
+    def get(self):
+        template_vars={'title':'RESUME','name':'resume'}	
+    	template = JINJA_ENVIRONMENT.get_template('resume.html')
+        self.response.out.write(template.render(template_vars))
+
+class AboutHandler(webapp2.RequestHandler):
+    def get(self):
+        template_vars={'title':'ABOUT','name':'aboutme'}	
+    	template = JINJA_ENVIRONMENT.get_template('aboutme.html')
+        self.response.out.write(template.render(template_vars))
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/index', MainHandler),
+    ('/photos', PhotosHandler),
+    ('/resume', ResumeHandler),
+    ('/aboutme', AboutHandler),
+    RedirectRoute('/<:.*>', redirect_to='/')
+], debug=False)
